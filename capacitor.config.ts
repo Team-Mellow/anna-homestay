ebce415a86f243a8df106ee7065e5e3803570062
farhanahaina <farhanahaina2002@gmail.com>
2023-10-31 01:18:03 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ANNA HOMESTAY',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
